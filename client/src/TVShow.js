529234785adbfecdd3a05ccf17d2fe5afebfbc6f
Ebere Okezie <ebereokezie@gmail.com>
2023-01-11 21:43:38 -0600

    add styling to first half of television show page

--- client/src/TVShow.js
+++ client/src/TVShow.js
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import TVShowInfo from './TVShowInfo';
 function TVShow({tvshows}) {
 
 
 const {slug} = useParams();
 
+console.log(slug)
+
+const [tvshow, setTvshow] = useState({})
+
+useEffect(()=> {
+    fetch( `/television_shows/${slug}`)
+    .then((data) => data.json())
+    .then(data => setTvshow(data))
+}, [])
 
 
-
-
-
-
+console.log(tvshow.reviews)
     return(
-        <>
-       { tvshows.filter((list) => list.slug === slug)
-        .map((list) => (
-        <div className="full-card" key={ list.id }>
-        <h2>Name: {list.title}</h2>
-        <h4>Category: {list.description}</h4>
+        <div className ="outer-column">
+            <div className='inner-column'>
+                <TVShowInfo tvshow ={tvshow} />
+                <div className = 'review-list'></div>
+            </div>
+            <div className='inner-column'>
+                <div className = "review-form"></div>
+            </div>
         </div>
-        ))}
-        </>
     )
     
 }
---
+++ client/src/TVShowInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function TVShowInfo({tvshow}) {
+ 
+    return (
+    <div className = "outer-wrapper">
+           <div className= "tvinfoPicture"><img src = {tvshow.picture_url} /></div>
+           <h1 className= "tvinfoTitle">{tvshow.title}</h1>
+           <h2 className= "tvinfoSeason">Season: {tvshow.season}</h2>
+           <h2 className= "tvinfoEpisode">Episode: {tvshow.episode}</h2>
+           <div className= "tvinfoDescription">Synopsis: {tvshow.description}</div>
+           <div className = "tvinfoAverageRating">Average Rating: {tvshow.average_rating}</div>
+           {/* <div className = "tvshowTotalReviews"> User reviews: {tvshow.reviews.length}</div> */}
+
+        
+    </div>
+    );
+  }
+
+  export default TVShowInfo
+
